7fb08b4bfe3f0d8c6e7a61cb3054c44254fb94d3
khalednouredine <khalednouredine9@gmail.com>
2025-08-24 08:17:54 +0100

    modifications apportées au projet

---
+++ resources/views/Accueil.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Page d'accueil</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, alias beatae. Voluptas voluptatibus id labore illum quasi, excepturi necessitatibus est veniam sed dolorum odit possimus unde vero quia eius magnam?</p>
+</body>
+</html>
