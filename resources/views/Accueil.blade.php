ebf468682f2e8fb22cf6b5cddea89a15aa520bd4
khalednouredine <khalednouredine9@gmail.com>
2025-08-24 09:20:37 +0100

    Premieres pages

--- resources/views/Accueil.blade.php
+++ resources/views/Accueil.blade.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Page d'accueil</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, alias beatae. Voluptas voluptatibus id labore illum quasi, excepturi necessitatibus est veniam sed dolorum odit possimus unde vero quia eius magnam?</p>
+    <a href="/publications">Publications</a>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Accueil');
 });
+Route::get('/publications', function () {
+    return view('Publications');
+});
